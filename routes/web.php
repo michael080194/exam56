58505a5fe231e0caae1b0f7a029e8655d933f7ef
michael080194 <michael080194@gmail.com>
2018-07-02 15:35:14 +0800

    修改首頁

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = ['name' => 'Michael', 'say' => '嗨！'];
+
+    return view('welcome', $data);
 });
 
 Auth::routes();
